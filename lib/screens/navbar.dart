1e22bae21d62a82cf76f0e70fa9db41eec05ed48
unknown <bhandari.krish.kb@gmail.com>
2024-12-23 15:24:39 +0530

    Added Simple Widgets to Drawer

--- lib/screens/navbar.dart
+++ lib/screens/navbar.dart
@@ -9,73 +9,42 @@ class Navbar extends StatefulWidget {
 }
 
 class _NavbarState extends State<Navbar> {
-
-  final double coverheight = 175;
-  final double profileheight = 120;
-
-
   @override
   Widget build(BuildContext context) {
-    final double top = coverheight - profileheight/2;
-
     return Drawer(
       backgroundColor: tdBGColor,
-      
 
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-
-          Stack(
-            alignment: Alignment.center,
-            clipBehavior: Clip.none,
-            
-            children: [
-              Container(
-                width: 400,
-                height: coverheight,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: ClipRect(                  
-                  clipBehavior: Clip.hardEdge ,
-                  child: Image.asset("assets/todo_banner.webp"),
-                ),
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: tdBlue,
+            ),
+            child: Text(
+              "Menu",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
               ),
-              
-              Center(
-                child: Positioned(
-                  top: top,
-                  child: CircleAvatar(
-                      radius: profileheight/2,
-                      child: ClipOval(
-                        child: Image.asset("assets/uttu.png"),
-                      ),
-                    ),
-                ),
-              ),
-            ],
-          )
+            ),
+          ),
 
-
-
-
-
-
-
-          // UserAccountsDrawerHeader(
-          //   accountName: Text("Utkrisht Bhandari", style: TextStyle(color: Colors.black)),
-          //   accountEmail: Text("bhandari.krish.kb@gmail.com",  style: TextStyle(color: Colors.black),),
-          //   currentAccountPicture: CircleAvatar(
-          //     // child: ClipOval(
-          //       child: Image.asset("assets/uttu.png"),
-          //     // ),
-          //   ),
-
-          //   decoration: BoxDecoration(
-          //     color: tdBGColor,
-          //   ),
-          // ),
-
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text("Settings"),
+            onTap: (){
+              print("Click Detected - Settings");
+            },
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text("About"),
+            onTap: (){
+              print("Click Detected - About");
+            },
+          ),
         ],
       ),
     );
